5341152043b90b122678935c55ea13d13d632df5
Arsil <arsilq@yahoo.com>
2021-06-11 21:39:05 +0700

    Title

--- src/pages/index.js
+++ src/pages/index.js
@@ -12,7 +12,7 @@ import Footer from "../components/Footer"
 
 const IndexPage = () => (
   <Layout>
-    <SEO title="Portfolio Template" />
+    <SEO title="Arsil Qodryantha" />
     <Header></Header>
     <Work></Work>
     <About></About>
