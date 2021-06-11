62a1ba40712bf7a8a180b1035263b070a3f5c073
Arsil <arsilq@yahoo.com>
2021-06-11 22:06:16 +0700

    Updated

--- src/yourdata.js
+++ src/yourdata.js
@@ -28,7 +28,7 @@ export default {
   ],
   //   Header Paragraph
   headerParagraph:
-    "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text.",
+    "LLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text.",
 
   //Contact Email
   contactEmail: "hello@chetanverma.com",
